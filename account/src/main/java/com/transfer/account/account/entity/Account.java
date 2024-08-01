ba177da7cb34a52421fd44104fbc41c72ea0c8c5
Muhammad Hussein <156622887+MuhammedHussein3@users.noreply.github.com>
2024-08-01 19:00:51 +0300

    Update Account entity
    - Added @Enumerated annotation for Branch and AccountType fields.
    - Changed @CreationTimestamp to @CreatedDate and @UpdateTimestamp to @LastModifiedDate for createdAt and updatedAt fields.
    - Ensured account_id, user_name, and account_number are unique and non-null.
    - Updated column names to match new naming conventions.

--- account/src/main/java/com/transfer/account/account/entity/Account.java
+++ account/src/main/java/com/transfer/account/account/entity/Account.java
@@ -1,4 +1,73 @@
 package com.transfer.account.account.entity;
 
+import com.transfer.account.account.dto.enums.AccountType;
+import com.transfer.account.account.dto.enums.Branch;
+import com.transfer.account.account.dto.enums.Country;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.EnumType.STRING;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "accounts")
 public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "account_id", nullable = false)
+    private Long id;
+
+    @Column(name = "user_name", nullable = false, unique = true)
+    private String userName;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "country", nullable = false)
+    @Enumerated(STRING)
+    private Country country;
+
+    @Column(name = "account_type", nullable = false)
+    @Enumerated(STRING)
+    private AccountType accountType;
+
+
+    @Column(name = "branch", nullable = false)
+    @Enumerated(STRING)
+    private Branch branch;
+
+    @Column(name = "account_number", nullable = false, unique = true)
+    private String accountNumber;
+
+    @Column(name = "balance", nullable = false)
+    private BigDecimal balance;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(name = "update_at", nullable = false)
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+
+
+
 }
