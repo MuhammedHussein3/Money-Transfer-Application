46864792955e8ec5616ad0928342ed1c94bfcf20
Muhammad Hussein <156622887+MuhammedHussein3@users.noreply.github.com>
2024-08-01 19:36:12 +0300

    Add createAccount method to AccountService and implementation in AccountServiceImpl
    - Added createAccount method to AccountService interface.
    - Implemented createAccount method in AccountServiceImpl.
    - Added logic to create and save a new Account entity from AccountCreateRequest

--- account/src/main/java/com/transfer/account/account/service/AccountServiceImpl.java
+++ account/src/main/java/com/transfer/account/account/service/AccountServiceImpl.java
@@ -1,7 +1,14 @@
 package com.transfer.account.account.service;
 
+import com.transfer.account.account.dto.request.AccountCreateRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AccountServiceImpl implements AccountService{
+    @Override
+    public void createAccount(AccountCreateRequest createRequest) {
+
+    }
 }
