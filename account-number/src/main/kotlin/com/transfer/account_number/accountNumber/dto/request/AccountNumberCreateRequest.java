7b527c8ba5e836de05387f0252bbdf4e8443bb8e
Muhammad Hussein <156622887+MuhammedHussein3@users.noreply.github.com>
2024-08-01 20:41:26 +0300

    Add AccountNumberCreateRequest record with validation
    - Created AccountNumberCreateRequest record for handling account number creation requests.
    - Added @NotBlank annotations to ensure accountType and branchCode are provided

---
+++ account-number/src/main/kotlin/com/transfer/account_number/accountNumber/dto/request/AccountNumberCreateRequest.java
@@ -0,0 +1,13 @@
+package com.transfer.account_number.accountNumber.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record AccountNumberCreateRequest(
+
+        @NotBlank(message = "AccountType must be required")
+        String accountType,
+
+        @NotBlank(message = "BranchCode must be required")
+        String branchCode
+) {
+}
