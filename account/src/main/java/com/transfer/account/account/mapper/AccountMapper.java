bb77c9e0916e84525d9152172e30ba9bdf910912
Muhammad Hussein <156622887+MuhammedHussein3@users.noreply.github.com>
2024-08-01 19:48:01 +0300

    Create account builder for user registration
    This commit adds a builder method for creating an account object,
    utilizing data from the createRequest parameter. This enhances
    the user registration process by ensuring all necessary fields
    are captured.

--- account/src/main/java/com/transfer/account/account/mapper/AccountMapper.java
+++ account/src/main/java/com/transfer/account/account/mapper/AccountMapper.java
@@ -1,9 +1,22 @@
 package com.transfer.account.account.mapper;
 
+import com.transfer.account.account.dto.request.AccountCreateRequest;
+import com.transfer.account.account.entity.Account;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountMapper {
 
 
+    public Account mapToAccount(AccountCreateRequest createRequest) {
+
+        return Account.builder()
+                .accountType(createRequest.accountType())
+                .email(createRequest.email())
+                .branch(createRequest.branch())
+                .userId(createRequest.userId())
+                .userName(createRequest.userName())
+                .country(createRequest.country())
+                .build();
+    }
 }
