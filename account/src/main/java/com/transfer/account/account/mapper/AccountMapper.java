bf322a733420bc36de577daffcc3e6343d969175
Muhammad Hussein <156622887+MuhammedHussein3@users.noreply.github.com>
2024-08-05 14:45:39 +0300

    Maps an Account entity to an AccountDetailsResponse DTO.   This method is responsible for converting the domain model object (Account)   into a data transfer object (AccountDetailsResponse) to be sent to the client.

--- account/src/main/java/com/transfer/account/account/mapper/AccountMapper.java
+++ account/src/main/java/com/transfer/account/account/mapper/AccountMapper.java
@@ -1,8 +1,11 @@
 package com.transfer.account.account.mapper;
 
 import com.transfer.account.account.dto.request.AccountCreateRequest;
+import com.transfer.account.account.dto.response.AccountDetailsResponse;
 import com.transfer.account.account.entity.Account;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 public class AccountMapper {
@@ -17,6 +20,21 @@ public class AccountMapper {
                 .userId(createRequest.userId())
                 .userName(createRequest.userName())
                 .country(createRequest.country())
+                .balance(BigDecimal.valueOf(0))
                 .build();
+    }
+
+    public AccountDetailsResponse mapToAccountDetailsResponse(Account account) {
+        return AccountDetailsResponse.builder()
+                .accountType(account.getAccountType().toString())
+                .userName(account.getUserName())
+                .accountNumber(account.getAccountNumber())
+                .email(account.getEmail())
+                .createdAt(account.getCreatedAt())
+                .branch(account.getBranch().toString())
+                .country(account.getCountry().toString())
+                .dateOfBirth(account.getDateOfrBirth())
+                .build();
+
     }
 }
