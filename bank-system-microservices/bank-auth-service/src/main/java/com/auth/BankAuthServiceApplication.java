c6ec31b00f21bd13dacd1234deec4f4a1fa3d2a0
Muhammad Hussein <156622887+MuhammedHussein3@users.noreply.github.com>
2024-08-13 10:00:23 +0300

    bank system microservices arch

---
+++ bank-system-microservices/bank-auth-service/src/main/java/com/auth/BankAuthServiceApplication.java
@@ -0,0 +1,26 @@
+package com.auth;
+
+import com.auth.entity.Role;
+import com.auth.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class BankAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAuthServiceApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository) {
+		return args -> {
+			if (roleRepository.findByName("USER").isEmpty()) {
+				roleRepository.save(Role.builder().name("USER").build());
+			}
+		};
+	}
+}
---
+++ bank-system-microservices/bank-notification-service/src/main/java/com/notification/BankNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankNotificationServiceApplication.class, args);
+	}
+
+}
